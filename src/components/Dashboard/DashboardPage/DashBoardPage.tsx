04fc55c0935c3a751eaf2c85fbbb68f3632e07f2
Dylan <dylangroux2105@gmail.com>
2026-02-26 00:51:44 +0100

    mini : refactor css and login page See #12

--- src/components/Dashboard/DashboardPage/DashBoardPage.tsx
+++ src/components/Dashboard/DashboardPage/DashBoardPage.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import sophieimg from '../../../assets/sophie.png'
+import './DashBoardPage.css'
 
 const DashboardPage: React.FC = () => {
   return (
-    <div>
-      
+    <div className='user-card'>
+      <div className='user-card-info'>
+        <img className='user-image' src={sophieimg} alt="Sophie" />
+        <div className='user-member-name'>
+          <h1>Sophie Martin</h1>
+          <p>Membre depuis le 14 juin 2023</p>
+        </div>
+      </div>
+      <div className='user-parcours'>
+        <span>Distance totale parcourue</span>
+        <div className='user-card-stats'>
+          <p>120 km</p>
+        </div>
+      </div>
     </div>
   )
 }
